377ea9fd7e77ed6ee06d723abd0970eb7cda8558
lucaslento92 <lucaslento92@gmail.com>
2025-10-23 12:03:32 -0300

    Segundo commit add report.html y enhancement en automation

--- conftest.py
+++ conftest.py
@@ -1,10 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from utils import login
 
 @pytest.fixture
 def driver():
-    driver= webdriver.Chrome()
+    # Configuración de Chrome
+    options = webdriver.ChromeOptions()
+
+    # 🔧 Desactivar el administrador de contraseñas y el aviso de seguridad de Google
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False
+    }
+    options.add_experimental_option("prefs", prefs)
+
+    # Opcional: abrir Chrome en modo incógnito (evita datos guardados previos)
+    options.add_argument("--incognito")
+
+    driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     yield driver
     driver.quit()
 
